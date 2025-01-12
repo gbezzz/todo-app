5ed065e5be69306e6bd56d72ddefcd9a9f9ac1e2
Prince Gbezeh <gbezehp@gmail.com>
2025-01-12 09:26:10 +0000

    Add todo collection logic

---
+++ src/core.js
@@ -0,0 +1,84 @@
+// check list factory
+const CheckList = function () {
+  const list = [];
+
+  const getList = () => list;
+
+  const updateList = (...items) => {
+    list.push(...items);
+    return list;
+  };
+
+  const emptyList = () => {
+    list.splice(0, list.length);
+    return list;
+  };
+
+  return { getList, updateList, emptyList };
+};
+
+// due date factory
+const DueDate = function () {
+  let dueDate = new Date();
+
+  const updateDate = (year, month, day) => {
+    dueDate = new Date(year, month, day);
+    return dueDate;
+  };
+
+  const getDate = () => dueDate;
+
+  return { getDate, updateDate };
+};
+
+// todos definition
+class Todo {
+  constructor(
+    title = "",
+    description = "",
+    priority = 0,
+    dueDate = DueDate().getDate(),
+    checkList = CheckList().getList()
+  ) {
+    this.title = title;
+    this.description = description;
+    this.priority = priority;
+    this.dueDate = dueDate;
+    this.checkList = checkList;
+  }
+}
+
+// todos collection
+const Projects = function () {
+  const projects = [];
+
+  //   todo is a property of a project that's an array
+  const addTodoToProject = (todo) => {
+    todo.push(new Todo());
+    return "New Todo Created";
+  };
+
+  const emptyProject = (todo) => {
+    todo.splice(0, todo.length);
+    return "Project emptied";
+  };
+
+  const defaultProject = (name = "default") => {
+    const todo = [];
+    return { name, todo, addTodoToProject, emptyProject };
+  };
+
+  const defaultTodo = defaultProject();
+  projects.push(defaultTodo);
+
+  function newProject(name = "New Project") {
+    const todo = [];
+    return { name, todo, addTodoToProject, emptyProject };
+  }
+
+  const createNewProject = () => projects.push(newProject());
+
+  const getProjects = () => projects;
+
+  return { getProjects, createNewProject };
+};
